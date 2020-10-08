a7f78d76a4dcebee81a2325f0dcc6573a476b838
Shawn Conerton <conerton52@gmail.com>
2020-10-08 09:27:33 -0500

    added for loop of fish on mainjs.

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,9 @@ import { useFish } from "./FishDataProvider.js"
 
 const allTheFish = useFish()
 
-console.log(allTheFish)
+// console.log(allTheFish)
+
+for (const fish of allTheFish) {
+    console.log(fish)
+}
+
